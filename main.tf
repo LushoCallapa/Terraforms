5d58857f533f9e776c3b4dc66edb46a3c28c1112
LushoCallapa <luiscallapa2004@gmail.com>
2025-12-10 22:24:25 -0400

    tarea final

--- main.tf
+++ main.tf
@@ -79,6 +79,7 @@ resource "aws_instance" "web" {
   subnet_id     = data.aws_subnets.selected.ids[0]
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
 
   user_data = <<EOF
 #!/bin/bash
